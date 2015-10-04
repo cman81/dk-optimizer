0845e687d8fcf9a8c852192e4abbab78d326063b
Christian Manalansan <christian.manalansan@cgifederal.com>
2015-10-04 05:13:34 -0400

    Allow for multiple simulations to be run Get user-generated randomized seed Do not increment threshold count for lowball teams

--- dk-optimizer.php
+++ dk-optimizer.php
@@ -29,6 +29,7 @@ $limits = array(
 );
 $threshold = 200;
 $depth = 1;
+$simulations = 1;
 
 // load player data
 if (file('data.csv') !== FALSE) {
@@ -41,8 +42,22 @@ $players = array_slice($players, 1); // remove header row
 // accept command-line arguments
 if (count($argv) > 1) {
   $args = array_slice($argv, 1);
-  list($threshold, $depth) = $args;
+  list($threshold, $depth, $simulations) = $args;
 }
+
+// get random seeds for each simulation
+if (PHP_OS == 'WINNT') {
+  echo 'Enter a line of random text (mash your keyboard): ';
+  $line = stream_get_line(STDIN, 1024, PHP_EOL);
+} else {
+  $line = readline('Enter a line of random text (mash your keyboard): ');
+}
+$seeds = array($line);
+for ($i = 0; $i < $simulations; $i++) {
+  $seeds[] = sha1(end($seeds));
+  echo "Seed added: " . end($seeds) . "\n";
+}
+$seeds = array_slice($seeds, 1);
 
 // remove all players with 0 projected points
 $trimmed_players = array();
@@ -101,103 +116,105 @@ if ($depth > 0) {
 }
 
 // run the engine
-$time_start = microtime_float();
-$i = 0;
-while ($i < $threshold) {
-  // initialize parameters
-  $closed_positions = array();
-  $current_team = array(
-    'total_points' => 0,
-    'total_salary' => 0,
+for ($j = 0; $j < $simulations; $j++) {
+  $time_start = microtime_float();
+  $i = 0;
+  while ($i < $threshold) {
+    // initialize parameters
+    $closed_positions = array();
+    $current_team = array(
+      'total_points' => 0,
+      'total_salary' => 0,
+    );
+    $current_limits = $limits;
+    $current_players = array();
+
+    // shuffle player 'cards'
+    foreach ($players as $value) {
+      $current_players[sha1(mt_rand(10000, 90000) . $seeds[$j])] = $value;
+    }
+    ksort($current_players);
+
+    // draft a team
+    foreach ($current_players as $value) {
+      // break out...
+      if (count($closed_positions) == 6) { // ...if we have a complete team
+        break;
+      }
+      if ($current_team['total_salary'] == $current_limits['salary']) { // ...if we have hit the salary cap
+        break;
+      }
+
+      // does adding this player put us over the salary cap?
+      if (($current_team['total_salary'] + $value[SALARY]) > $current_limits['salary']) {
+        continue;
+      }
+
+      // do we have a position slot for him?
+      if ($current_limits[$value[POSITION]]) { // e.g.: if we draw a QB, check against $limits[QB]
+        $current_limits[$value[POSITION]]--;
+        if ($current_limits[$value[POSITION]] == 0) {
+          $closed_positions[] = $value[POSITION];
+        }
+      } elseif ($current_limits[FLEX] && in_array($value[POSITION], array(RB, WR, TE))) { // do we have a flex slot?
+        $current_limits[FLEX]--;
+        if ($current_limits[FLEX] == 0) {
+          $closed_positions[] = FLEX;
+        }
+      } else {
+        continue;
+      }
+
+      // add to our team
+      $current_team['players'][] = $value;
+      $current_team['total_points'] += $value[POINTS];
+      $current_team['total_salary'] += $value[SALARY];
+    }
+
+    // is this team the best we have assembled?
+    if ($current_team['total_points'] > $best_team['total_points']) {
+      $best_team = $current_team;
+      $i = round($i / 2);
+      echo '+';
+    } elseif ($current_team['total_salary'] > $limits['salary'] - 500) {
+      $i++;
+    }
+
+    if ($i % round($threshold / 10) == 0) {
+      echo $i / round($threshold / 10);
+    }
+  }
+  $time_end = microtime_float();
+  $time = $time_end - $time_start;
+  echo "\n";
+
+  usort(
+    $best_team['players'],
+    function($a, $b) {
+      if ($a[POINTS] == $b[POINTS]) {
+        return ($a[SALARY] > $b[SALARY]) ? -1 : 1;
+      }
+      return ($a[POINTS] > $b[POINTS]) ? -1 : 1;
+    }
   );
-  $current_limits = $limits;
-  $current_players = array();
 
-  // shuffle player 'cards'
-  foreach ($players as $value) {
-    $current_players[sha1(microtime(TRUE) . mt_rand(10000, 90000))] = $value;
-  }
-  ksort($current_players);
-
-  // draft a team
-  foreach ($current_players as $value) {
-    // break out...
-    if (count($closed_positions) == 6) { // ...if we have a complete team
-      break;
-    }
-    if ($current_team['total_salary'] == $current_limits['salary']) { // ...if we have hit the salary cap
-      break;
-    }
-
-    // does adding this player put us over the salary cap?
-    if (($current_team['total_salary'] + $value[SALARY]) > $current_limits['salary']) {
-      continue;
-    }
-    
-    // do we have a position slot for him?
-    if ($current_limits[$value[POSITION]]) { // e.g.: if we draw a QB, check against $limits[QB]
-      $current_limits[$value[POSITION]]--;
-      if ($current_limits[$value[POSITION]] == 0) {
-        $closed_positions[] = $value[POSITION];
-      }
-    } elseif ($current_limits[FLEX] && in_array($value[POSITION], array(RB, WR, TE))) { // do we have a flex slot?
-      $current_limits[FLEX]--;
-      if ($current_limits[FLEX] == 0) {
-        $closed_positions[] = FLEX;
-      }
-    } else {
-      continue;
-    }
-    
-    // add to our team
-    $current_team['players'][] = $value;
-    $current_team['total_points'] += $value[POINTS];
-    $current_team['total_salary'] += $value[SALARY];
+  $output = array();
+  foreach ($best_team['players'] as $value) {
+    echo str_pad($value[NAME] . ' ', 25) . str_pad('(' . $value[POSITION] . ')', 5) . ' cost = ' . str_pad('$' . $value[SALARY], 6) . ' points = ' . str_pad($value[POINTS], 4) . "\n";
+    $output[] = $value[NAME];
   }
 
-  // is this team the best we have assembled?
-  if ($current_team['total_points'] > $best_team['total_points']) {
-    $best_team = $current_team;
-    $i = round($i / 2);
-    echo '+';
-  } else {
-    $i++;
-  }
+  echo "Total Salary: $" . $best_team['total_salary'] . "\n";
+  $output[] = $best_team['total_salary'];
 
-  if ($i % round($threshold / 10) == 0) {
-    echo $i / round($threshold / 10);
-  }
+  echo "Total Points: " . $best_team['total_points'] . "\n";
+  $output[] = $best_team['total_points'];
+
+  echo "Execution time: " . $time . " seconds\n";
+  $file = fopen('output.csv', 'a');
+  fwrite($file, implode(',', $output) . "\n");
+  fclose($file);
 }
-$time_end = microtime_float();
-$time = $time_end - $time_start;
-echo "\n";
-
-usort(
-  $best_team['players'],
-  function($a, $b) {
-    if ($a[POINTS] == $b[POINTS]) {
-      return ($a[SALARY] > $b[SALARY]) ? -1 : 1;
-    }
-    return ($a[POINTS] > $b[POINTS]) ? -1 : 1;
-  }
-);
-
-$output = array();
-foreach ($best_team['players'] as $value) {
-  echo str_pad($value[NAME] . ' ', 25) . str_pad('(' . $value[POSITION] . ')', 5) . ' cost = ' . str_pad('$' . $value[SALARY], 6) . ' points = ' . str_pad($value[POINTS], 4) . "\n";
-  $output[] = $value[NAME];
-}
-
-echo "Total Salary: $" . $best_team['total_salary'] . "\n";
-$output[] = $best_team['total_salary'];
-
-echo "Total Points: " . $best_team['total_points'] . "\n";
-$output[] = $best_team['total_points'];
-
-echo "Execution time: " . $time . " seconds\n";
-$file = fopen('output.csv', 'a');
-fwrite($file, implode(',', $output) . "\n");
-fclose($file);
 
 function microtime_float() {
     list($usec, $sec) = explode(" ", microtime());
